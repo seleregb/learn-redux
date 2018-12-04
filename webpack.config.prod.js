1ef7b65fe4efd8bcccc55dc91157f75b52eb23d7
186866380 <olugbenro.selere@rbc.com>
2018-12-04 13:14:44 -0500

    prepared production build

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -5,7 +5,8 @@ var CopyWebpackPlugin = require('copy-webpack-plugin');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var MiniCssExtractPlugin = require("mini-css-extract-plugin");
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
-const ReactRootPlugin = require("html-webpack-react-root-plugin");
+var ReactRootPlugin = require("html-webpack-react-root-plugin");
+var postStylus = require('poststylus');
 
 module.exports = {
   mode: 'production',
@@ -22,6 +23,7 @@ module.exports = {
     filename: "[name].bundle.js"
   },
   plugins: [
+    // new ExtractTextPlugin('styles.css'),
     new HtmlWebpackPlugin({
       title: 'Learn Redux',
       inject: 'body'
@@ -31,6 +33,12 @@ module.exports = {
       'process.env': {
         'NODE_ENV': 'production'
       }
+    }),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: "[name].css",
+      chunkFilename: "[id].css"
     })
   ],
   optimization: {
@@ -45,7 +53,15 @@ module.exports = {
     //   })
     // ],
     splitChunks: {
-      chunks: 'all'
+      chunks: 'all',
+      cacheGroups: {
+        styles: {
+          name: 'styles',
+          test: /\.styl$/,
+          chunks: 'all',
+          enforce: true
+        }
+      }
     }
   },
   module: {
@@ -63,9 +79,29 @@ module.exports = {
       // CSS
       {
         test: /\.styl$/,
-        include: path.join(__dirname, 'client'),
-        use: ['style-loader', 'css-loader', 'stylus-loader']
-      }
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'stylus-loader',
+            options: {
+              use: [
+                postStylus(['autoprefixer']),
+              ],
+              modules: true,
+            },
+          },
+          // {
+          //   loader: MiniCssExtractPlugin.loader,
+          //   options: {
+          //     // you can specify a publicPath here
+          //     // by default it use publicPath in webpackOptions.output
+          //     publicPath: '../'
+          //   },
+          // }
+        ],
+        exclude: /node_modules/,
+      },
     ]
   }
 };
